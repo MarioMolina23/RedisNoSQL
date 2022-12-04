16420077b2d7cd305a97cd0243fc54ba2adb663a
chrisaguero <chris23298@gmail.com>
2022-12-03 20:44:40 -0600

    api Rest

---
+++ Api-Rest/Clases/Clases/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Clases
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrueba = new System.Windows.Forms.Button();
+            this.btnPost = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAlmacenamiento = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textDate = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textColor = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPrueba
+            // 
+            this.btnPrueba.Location = new System.Drawing.Point(15, 277);
+            this.btnPrueba.Name = "btnPrueba";
+            this.btnPrueba.Size = new System.Drawing.Size(295, 53);
+            this.btnPrueba.TabIndex = 0;
+            this.btnPrueba.Text = "GET";
+            this.btnPrueba.UseVisualStyleBackColor = true;
+            this.btnPrueba.Click += new System.EventHandler(this.btnPrueba_Click);
+            // 
+            // btnPost
+            // 
+            this.btnPost.Location = new System.Drawing.Point(333, 277);
+            this.btnPost.Name = "btnPost";
+            this.btnPost.Size = new System.Drawing.Size(295, 53);
+            this.btnPost.TabIndex = 2;
+            this.btnPost.Text = "post";
+            this.btnPost.UseVisualStyleBackColor = true;
+            this.btnPost.Click += new System.EventHandler(this.btnPost_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Modelo";
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(96, 71);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(237, 20);
+            this.txtModelo.TabIndex = 4;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(96, 97);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(237, 20);
+            this.txtPrecio.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Precio";
+            // 
+            // txtAlmacenamiento
+            // 
+            this.txtAlmacenamiento.Location = new System.Drawing.Point(96, 123);
+            this.txtAlmacenamiento.Name = "txtAlmacenamiento";
+            this.txtAlmacenamiento.Size = new System.Drawing.Size(237, 20);
+            this.txtAlmacenamiento.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Almacenamiento";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textDate);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textColor);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtMarca);
+            this.groupBox1.Controls.Add(this.txtAlmacenamiento);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtModelo);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(142, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(357, 219);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Computadoras";
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(96, 45);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(237, 20);
+            this.txtMarca.TabIndex = 13;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(96, 19);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(237, 20);
+            this.txtID.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Marca";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ID";
+            // 
+            // textDate
+            // 
+            this.textDate.Location = new System.Drawing.Point(96, 175);
+            this.textDate.Name = "textDate";
+            this.textDate.Size = new System.Drawing.Size(237, 20);
+            this.textDate.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Date";
+            // 
+            // textColor
+            // 
+            this.textColor.Location = new System.Drawing.Point(96, 149);
+            this.textColor.Name = "textColor";
+            this.textColor.Size = new System.Drawing.Size(237, 20);
+            this.textColor.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Color";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(646, 350);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnPost);
+            this.Controls.Add(this.btnPrueba);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrueba;
+        private System.Windows.Forms.Button btnPost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAlmacenamiento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textColor;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Api-Rest/Clases/Clases/Form1.cs
@@ -0,0 +1,62 @@
+﻿using Clases.ApiRest;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clases
+{
+    public partial class Form1 : Form
+    {
+        DBApi dBApi = new DBApi();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrueba_Click(object sender, EventArgs e)
+        {
+            dynamic respuesta = dBApi.Get("http://localhost:8000/Proyecto_redis/get/00d4ecd0-b83b-40a9-9367-8578ff3f8d21");
+            txtID.Text = respuesta.id.ToString();
+            txtMarca.Text = respuesta.marca.ToString();
+            txtModelo.Text = respuesta.modelo.ToString();
+            txtPrecio.Text = respuesta.precio.ToString();
+            txtAlmacenamiento.Text = respuesta.almacenamiento.ToString();
+            textColor.Text = respuesta.color.ToString();
+            textDate.Text = respuesta.date.ToString();
+        }
+
+        private void btnPost_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona
+            {
+                job = txtMarca.Text,
+                name = txtID.Text
+            };
+
+            string json = JsonConvert.SerializeObject(persona);
+
+            dynamic respuesta = dBApi.Post("https://reqres.in/api/users",json);
+
+            MessageBox.Show(respuesta.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Persona
+    {
+        public string name { get; set; }
+        public string job { get; set; }
+    }
+}
+
